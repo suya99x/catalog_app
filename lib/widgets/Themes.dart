4d2872faef83a0d2fb5db71e797c309c9c5febd2
sudin shakya <sudinshk@gmail.com>
2021-05-29 10:23:06 +0545

    day 5 completed

---
+++ lib/widgets/Themes.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme{
+  static ThemeData LightTheme(BuildContext context) => ThemeData(
+
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+        appBarTheme: AppBarTheme(
+//to remove primary color in app bar
+          color: Colors.white,
+//to remove shadow
+          elevation: 0.0,
+//to show icon which is white due to removal of background
+          iconTheme: IconThemeData(color: Colors.black),
+          textTheme: Theme.of(context).textTheme,
+        ));
+
+  static ThemeData DarkTheme(BuildContext context) => ThemeData(
+
+      primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      primaryTextTheme: GoogleFonts.latoTextTheme(),
+      appBarTheme: AppBarTheme(
+//to remove primary color in app bar
+        color: Colors.black,
+//to remove shadow
+        elevation: 0.0,
+//to show icon which is white due to removal of background
+        iconTheme: IconThemeData(color: Colors.white),
+        textTheme: Theme.of(context).textTheme,
+      ));
+
+}
