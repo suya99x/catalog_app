eec553ee1573b527506dcc888130cefa449506c4
sudin shakya <sudinshk@gmail.com>
2021-05-29 19:18:21 +0545

    day7

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -42,64 +42,58 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
 
-  //  final itemlist=List.generate(20, (index) => CatalogModel.items[0]);
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       centerTitle: true,
-  //       title: Text('Catalog App'),
-  //     ),
-  //     body: Text(context.runtimeType.toString()),
-  //     body: Padding(
-  //       padding: const EdgeInsets.all(16.0),
-  //       //to fix null error in getter in lisstview
-  //       child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)
-  //           ? GridView.builder(
-  //             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-  //               crossAxisCount: 2,
-  //               //mainaxisspacing spaces from vertical
-  //               mainAxisSpacing: 16,
-  //               crossAxisSpacing: 16,
-  //             ),
-  //         // itemBuilder: (context, index) => ItemWidget(
-  //         //       item: CatalogModel.items[index]),
-  //         itemBuilder: (context, index){
-  //           final item = CatalogModel.items[index];
-  //           return Card(
-  //             clipBehavior: Clip.antiAlias,
-  //             shape: RoundedRectangleBorder(
-  //               borderRadius: BorderRadius.circular(10),
-  //             ),
-  //             child: GridTile(
-  //               header:  Container(
-  //                 child: Text(item.name),
-  //                 decoration: BoxDecoration(
-  //                   color: Colors.deepPurple
-  //                 ),
-  //                 padding: const EdgeInsets.all(16),
-  //                 ),
-  //               footer: Container(
-  //                 child: Text("\$${item.price}"),
-  //                  decoration: BoxDecoration(
-  //                   color: Colors.blue
-  //                 ),
-  //                 ),
-  //               child: Image.network(item.image)));
-  //         },
-  //           itemCount: CatalogModel.items.length,
-
-  //       )
-
-  //        :Center(//if item not loaded in list view
-  //         child: CircularProgressIndicator(),
-  //       )
-  //     ),
-  //     drawer: MyDrawer(),
+   //final itemlist=List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
-      body: Column[
-        "Catalog App".text.make(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Catalog App'),
+      ),
+      //body: Text(context.runtimeType.toString()),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        //to fix null error in getter in lisstview
+        child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                //mainaxisspacing spaces from vertical
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+              ),
+          // itemBuilder: (context, index) => ItemWidget(
+          //       item: CatalogModel.items[index]),
+          itemBuilder: (context, index){
+            final item = CatalogModel.items[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: GridTile(
+                header:  Container(
+                  child: Text(item.name),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple
+                  ),
+                  padding: const EdgeInsets.all(16),
+                  ),
+                footer: Container(
+                  child: Text("\$${item.price}"),
+                   decoration: BoxDecoration(
+                    color: Colors.blue
+                  ),
+                  ),
+                child: Image.network(item.image)));
+          },
+            itemCount: CatalogModel.items.length,
 
-      ],
-    )
+        )
+         :Center(//if item not loaded in list view
+          child: CircularProgressIndicator(),
+        )
+      ),
+      drawer: MyDrawer(),
+  
     );
   }
 }
