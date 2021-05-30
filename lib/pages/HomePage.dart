2056ef30a02ca6f7449a8d3e0ba2489cac286b5c
sudin shakya <sudinshk@gmail.com>
2021-05-30 17:06:16 +0545

    day12 fab state

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,3 +1,5 @@
+import 'package:catalog_app/core/store.dart';
+import 'package:catalog_app/modals/cart.dart';
 import 'package:catalog_app/modals/catalog.dart';
 import 'package:catalog_app/widgets/home_widgets/Catalog_header.dart';
 import 'package:catalog_app/widgets/home_widgets/Catalog_list.dart';
@@ -5,7 +7,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:velocity_x/velocity_x.dart';
-import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 import '../MyRoutes.dart';
 
@@ -15,6 +17,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // 1st method
+  //var url =Uri.https(
+  //   'api.jsonbin.io',
+  //    '/b/604dbddb683e7e079c4eefd3', {'q': '{http}'});
+//or 2nd method 
+final url = "https://api.jsonbin.io/b/604dbddb683e7e079c4eefd3";
+
   @override
   void initState() {
     super.initState();
@@ -24,8 +33,13 @@ class _HomePageState extends State<HomePage> {
   loadData() async {
     //to load animation of progess circular && to fix error listview
     await Future.delayed((Duration(seconds: 2)));
-    final catalogJson =
-        await rootBundle.loadString("assets/files/catalog.json");
+    // final catalogJson =
+    //     await rootBundle.loadString("assets/files/catalog.json");
+    //1st method
+    // final response =await http.get(url);
+    //2nd method
+     final response =await http.get(Uri.parse(url));
+    final catalogJson = response.body;
     //print(catalogjson); prints all json data use breakpoint to check(leftclick on line no) {...}
     // we cant load object directly so we decode to get object in dynamic format .ie key,value in map
     final decodeData = jsonDecode(catalogJson);
@@ -42,17 +56,31 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     //final itemlist=List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       //if not using velocityx theme.of(context).cardcolor,
       backgroundColor: context.canvasColor,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
-        child: Icon(CupertinoIcons.cart,
-        color: Colors.white,
-        ),
-        backgroundColor: context.theme.buttonColor,
-        ),
+      floatingActionButton: VxBuilder(
+        mutations: {AddMutation, RemoveMutation},
+        builder: (context, store, status) {
+          return FloatingActionButton(
+            onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+            child: Icon(
+              CupertinoIcons.cart,
+              color: Colors.white,
+            ),
+            backgroundColor: context.theme.buttonColor,
+          ).badge(
+              color: Vx.red500,
+              size: 20,
+              count: _cart.items.length,
+              textStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: context.accentColor,
+              ));
+        },
+      ),
       body: SafeArea(
         //mathi ra talako like battery nav button
         child: Container(
@@ -62,10 +90,9 @@ class _HomePageState extends State<HomePage> {
               CatalogHeader(),
               if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
                 CatalogList().py(16).expand()
-               else
-               CircularProgressIndicator().centered().expand(),
-               //Expanded(child: Center(child: CircularProgressIndicator()))
-                
+              else
+                CircularProgressIndicator().centered().expand(),
+              //Expanded(child: Center(child: CircularProgressIndicator()))
             ],
           ).px(16),
         ),
@@ -73,9 +100,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-
-
-
-
-
